72367228dc61ad404b8e325bc32a34c066d638a4
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2023-10-05 00:12:24 +0500

    - добавлен код для отрисовки рандомной траектории

--- app/src/main/java/gb/com/cartrajectoryanimation/view/DrawingView.kt
+++ app/src/main/java/gb/com/cartrajectoryanimation/view/DrawingView.kt
@@ -4,10 +4,13 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.DashPathEffect
+import android.graphics.LinearGradient
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.PathMeasure
 import android.graphics.PointF
+import android.graphics.Shader
 import android.util.AttributeSet
 import android.view.View
 
@@ -17,9 +20,18 @@ class DrawingView(
 ) : View(context, attrs) {
 
     private val pathPaint: Paint = Paint().apply {
-        color = Color.BLACK
         style = Paint.Style.STROKE
-        strokeWidth = 5f
+        strokeWidth = 20f
+        pathEffect = DashPathEffect(floatArrayOf(30f, 10f), 0f)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        val lightBlue = Color.parseColor("#ADD8E6")
+        val gradient = LinearGradient(0f, 0f, width.toFloat(), height.toFloat(),
+            lightBlue, Color.CYAN, Shader.TileMode.CLAMP)
+        pathPaint.shader = gradient
     }
 
     private val carPaint: Paint = Paint().apply {
@@ -34,10 +46,22 @@ class DrawingView(
         path.reset()
         path.moveTo(0f, 0f)
 
-        for(i in 1..4) {
-            val x = (width / 5f) * i
-            val y = (Math.random() * height).toFloat()
-            path.lineTo(x,y)
+        var x = 0f
+        var y = 0f
+        val xStep = width / 10f
+        val yStep = width / 10f
+
+        for (i in 1..20) {
+            if (i % 2 == 1) {
+                x += (if (Math.random() < 0.5) -1 else 1) * (xStep + (Math.random() * xStep - xStep / 2)
+                    .toFloat())
+                x = x.coerceAtLeast(0f).coerceAtMost(width.toFloat())
+            } else {
+                y += (if (Math.random() < 0.5) -1 else 1) * (yStep + (Math.random() * height - yStep / 2)
+                    .toFloat())
+                y = y.coerceAtLeast(0f).coerceAtMost(height.toFloat())
+            }
+            path.lineTo(x, y)
         }
         path.lineTo(width.toFloat(), height.toFloat())
         invalidate()
@@ -49,7 +73,7 @@ class DrawingView(
         canvas?.drawCircle(carPosition.x, carPosition.y, 20f, carPaint)
     }
 
-    fun setCarPosition(fraction: Float) {
+    private fun setCarPosition(fraction: Float) {
 
         val pathMeasure = PathMeasure(path, false)
         val pathLength = pathMeasure.length
@@ -63,7 +87,7 @@ class DrawingView(
 
     fun animateCarAlongPath() {
         val animator = ValueAnimator.ofFloat(0f, 1f).apply {
-            duration = 5000
+            duration = 10000
             addUpdateListener {
                 val fraction = it.animatedValue as Float
                 setCarPosition(fraction)
